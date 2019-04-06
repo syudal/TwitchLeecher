3cc12cf24342384c32cd5a1f3b0f12f4677dd847
syudal <syudal@smail.kongju.ac.kr>
2019-04-06 22:37:58 +0900

    Update to FFmpeg 4.1.1, Xceed 3.5.0

--- TwitchLeecher/GlobalAssemblyInfo.cs
+++ TwitchLeecher/GlobalAssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Twitch Leecher")]
-[assembly: AssemblyDescription("트위치 방송 다운로더")]
+[assembly: AssemblyDescription("트위치 영상 & 클립 다운로더")]
 [assembly: AssemblyCompany("Franiac & Syudal")]
 [assembly: AssemblyCopyright("Copyright ?2018 Dominik Rebitzer")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.5.5.0")]
+[assembly: AssemblyFileVersion("1.5.6.0")]
 [assembly: ComVisible(false)]
